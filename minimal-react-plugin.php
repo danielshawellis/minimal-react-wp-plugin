f10ce6cf2225ddea6bfc021d1f6d2de3cf573852
Daniel Ellis <danielshawellis@gmail.com>
2019-12-27 14:06:11 -0600

    Add shortcode to pull in React

--- minimal-react-plugin.php
+++ minimal-react-plugin.php
@@ -5,7 +5,27 @@
  * Author:            Daniel Ellis
  */
 
-// Basic security environment checks
+/*
+  Basic security environment check
+*/
 if ( ! defined( 'ABSPATH' ) ) {
  die;
 }
+
+/*
+  Load React app through a shortcode
+*/
+// Register react script
+function minimal_react_register_scripts() {
+  wp_register_script('minimal-react-plugin', plugins_url( 'assets/js/minimal-react-plugin.js', __FILE__ ), array (), false, false);
+}
+add_action('wp_enqueue_scripts', 'minimal_react_register_scripts');
+
+// Add shortcode through callback
+function minimal_react_shortcode_callback() {
+  // Echo the div that React will target as its container
+  echo "<div id='minimal-react-plugin'></div>";
+  // Enqueue the script only if shortcode callback runs
+  wp_enqueue_script('minimal-react-plugin');
+}
+add_shortcode( 'minimal_react_shortcode', 'minimal_react_shortcode_callback' );
