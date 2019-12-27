ed98d2f9a389f907354802bd657e44f7477d687f
Daniel Ellis <danielshawellis@gmail.com>
2019-12-27 11:38:09 -0600

    Add basic security

---
+++ minimal-react-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:       Minimal React Plugin
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ */
+
+// Basic security environment checks
+if ( ! defined( 'ABSPATH' ) ) {
+ die;
+}
